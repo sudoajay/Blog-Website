41511da9f123ef1e51e9041dba8d059833ca7982
SudoAjay <sudoajay@gmail.com>
2023-04-11 07:55:49 +0530

    changes

--- blog/models.py
+++ blog/models.py
@@ -2,15 +2,22 @@ from django.db import models
 from datetime import date
 
 # Create your models here.
-class addPost(models.Model):
-    slug = models.CharField(max_length=30, null=True)
-    image = models.CharField(max_length=15, null=True)
-    author = models.CharField(max_length=15, null=True)
-    date = models.DateField(("Date"), default=date.today)
-    title =models.CharField(max_length=30, null=True)
-    shortContent = models.TextField(null=True)
-    content = models.TextField(null=True)
-    def __str__(self):
-	    return self.slug
+# class addPost(models.Model):
+#     slug = models.CharField(max_length=30, null=True)
+#     image = models.CharField(max_length=15, null=True)
+#     author = models.CharField(max_length=15, null=True)
+#     date = models.DateField(("Date"), default=date.today)
+#     title =models.CharField(max_length=30, null=True)
+#     shortContent = models.TextField(null=True)
+#     content = models.TextField(null=True)
+    
+#     def __str__(self):
+# 	    return self.slug
+
+class ToDoList(models.Model):
+	name = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name
 
 
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,13 @@ from datetime import date
 
 from django.shortcuts import render
 
-from .models import addPost
+# from .models import addPost
+
+all_posts = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
 
 
 def get_date(post):
@@ -11,23 +17,23 @@ def get_date(post):
 
 # Create your views here.
 def starting_page(request):
-    allpost = addPost.objects.all()
-    # sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = allpost[-3:]if(len(allpost) >3) else allpost
+    # allpost = addPost.objects.all()
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = all_posts[-3:]if(len(all_posts) >3) else all_posts
     return render(request, "blog/index.html",{
         "posts": latest_posts
     })
     
 def posts(request):
-    allpost = addPost.objects.all()
+    # allpost = addPost.objects.all()
     return render(request,"blog/all-posts.html",{
-        "all_posts": allpost
+        "all_posts": all_posts
 
     })
     
 def post_detail(request,slug):
-    getPost= addPost.objects.get(slug = slug) 
-    # identified_post = next(post for post in all_posts if post['slug'] == slug)
+    # getPost= addPost.objects.get(slug = slug) 
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
     return render(request,"blog/post-detail.html", {
-        "post": getPost
+        "post": all_posts
     })
