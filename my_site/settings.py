1b7c6fda8881231b39f1dd858e63b03c19781fa4
SudoAjay <sudoajay@gmail.com>
2023-04-14 13:45:36 +0530

    small chnages but important

--- my_site/settings.py
+++ my_site/settings.py
@@ -10,13 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-from os.path import join, dirname
 from pathlib import Path
-from dotenv import load_dotenv
-
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-n4)%x5lf72w4c6^w7a9%6e=7c4z*y)g7r(15)&=p=y(9egj__g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG',False)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
